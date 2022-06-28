2a1f76eff9e67a441ed95898137131d631ef6115
naiduadada <108125335+naiduadada@users.noreply.github.com>
2022-06-28 20:44:48 +0530

    Update TicTacToeGame.java

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -24,28 +24,6 @@ printBoard(board);
 scanner.close();
 }
  
-private static void printBoard(char[][] board) {
-System.out.print((board[0][0]));
-System.out.print("|");
-System.out.print((board[0][1]));
-System.out.print("|");
-System.out.println((board[0][2]));
-System.out.println("-----");
-System.out.print((board[1][0]));
-System.out.print("|");
-System.out.print((board[1][1]));
-System.out.print("|");
-System.out.println((board[1][2]));
-System.out.println("-----");
-System.out.print((board[2][0]));
-System.out.print("|");
-System.out.print((board[2][1]));
-System.out.print("|");
-System.out.println((board[2][2]));
-}
-}
-
-
 private static boolean isGameFinished(char[][] board) {
 if (hasContestantWon(board, 'X')) {
 printBoard(board);
@@ -172,4 +150,26 @@ default:
 System.out.println(":(");
 }
 }
+ 
+ 
+private static void printBoard(char[][] board) {
+System.out.print((board[0][0]));
+System.out.print("|");
+System.out.print((board[0][1]));
+System.out.print("|");
+System.out.println((board[0][2]));
+System.out.println("-----");
+System.out.print((board[1][0]));
+System.out.print("|");
+System.out.print((board[1][1]));
+System.out.print("|");
+System.out.println((board[1][2]));
+System.out.println("-----");
+System.out.print((board[2][0]));
+System.out.print("|");
+System.out.print((board[2][1]));
+System.out.print("|");
+System.out.println((board[2][2]));
+}
+} 
  
